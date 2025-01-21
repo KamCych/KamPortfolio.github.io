9fe040604b86f155e59f82a3c8c5d3328e6caf35
kamChum <cychowskik@gmail.com>
2025-01-21 21:12:25 +0000

    Added slide indicator, added unity webgl build

--- slideshow.js
+++ slideshow.js
@@ -3,20 +3,23 @@ const img1 = document.getElementById("img-1")
 const img2 = document.getElementById("img-2")
 const img3 = document.getElementById("img-3")
 
-const slide_buttons = document.getElementById("slide-buttons");
+const slide_buttons_container = document.getElementById("slide-buttons-container");
+const slide_buttons = document.getElementsByClassName("slide-buttons");
 
 let current = 1;
 
 slideshow[1].style.display = "none";
 slideshow[2].style.display = "none";
-
+slide_buttons[0].style.backgroundColor = "#FF0000"
 function update_slide() {
 
     for (let i = 0; i < 3; i++) {
         slideshow[i].style.display = "none";
+        slide_buttons[i].style.backgroundColor = "#F1A01D";
     }
     
     slideshow[current].style.display = "block";
+    slide_buttons[current].style.backgroundColor = "#FF0000";
 
     current += 1
 
